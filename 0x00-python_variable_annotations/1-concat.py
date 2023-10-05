6f9977c98ef8025e0552843397167ec46eb48fdc
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-05 20:35:57 +0000

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+ type-annotated function concat that takes a
+ string str1 and a string str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """function concat that takes a string
+    str1 and a string str2
+    """
+    res = str1 + str2
+    return res
