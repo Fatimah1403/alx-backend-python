5eb37d0003e87eb6ed2e5be96fa8a14166d73d94
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-05 19:54:05 +0000

    Task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,9 +5,9 @@ float b as arguments and returns their sum as a float.
 
 
 def add(a: float, b: float) -> float:
-   """
-   function add that takes a float a
-   and a float b as arguments and returns their
-   sum as a float
-   """
-   return (a + b)
+    """
+    function add that takes a float a
+    and a float b as arguments and returns their
+    sum as a float
+    """
+    return (a + b)
