413b6687ff98dfd3cbb2ba0805a1f222a8564823
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-05 20:47:46 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+ type-annotated function floor which takes a float n
+"""
+
+
+def floor(n: float) -> int:
+    """
+    function floor which takes a float n as argument
+    """
+    return int(n)
