b9b3d754a4746a2af95b008bd7d7fca73b838579
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-10 21:35:21 +0000

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,20 +5,12 @@ coroutine called async_comprehension that takes no arguments
 """
 import asyncio
 import random
-from typing import Generator
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_generator() -> Generator[float, None, None]:
-    """each time asynchronously wait 1 second,
-    then yield a random number between 0 and 10
-
-    """
-    for i in range(10):
-        await asyncio.sleep(1)
-        yield random.random() * 10
-
-
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """ return the 10 random numbers."""
     rand_nos = [number async for number in async_generator()]
     return rand_nos
