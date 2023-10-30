88227a0c8b65041af552687179d7e18a571d6e4a
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-30 23:20:09 +0000

    New update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,34 +1,30 @@
 #!/usr/bin/env python3
-""" Unittest module """
-
+""" Unit Test and Integration Test"""
 import unittest
-from unittest.mock import patch, Mock, PropertyMock
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
+from unittest.mock import patch, PropertyMock
 
-from client import GithubOrgClient
+
+GithubOrgClient = __import__("client").GithubOrgClient
+TEST_PAYLOAD = __import__("fixtures").TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ Class for testing GithubOrgClient """
+    """TestGithubOrgClient class"""
 
-    @parameterized.expand([
-        ("google"),
-        ("abc"),
-    ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ Test method returns correct output """
-        endpoint = 'https://api.github.com/orgs/{}'.format(org_name)
-        spec = GithubOrgClient(data)
-        spec.org()
-        mock_json.assert_called_once_with(endpoint)
-
-    @parameterized.expand([
-        ("random-url", {'repos_url': 'http://some_url.com'})
-    ])
-    def test_public_repos_url(self, name, result):
-        """ Test method returns correct output """
-        with patch('client.GithubOrgClient.org',
-                   PropertyMock(return_value=result)):
-            response = GithubOrgClient(name)._public_repos_url
-            self.assertEqual(response, result.get('repos_url'))
+    @parameterized.expand(
+        [
+            ("google",),
+            ("abc",),
+        ]
+    )
+    @patch("client.get_json")
+    def test_org(self, org_name, mock_get_json):
+        """Test org method"""
+        dict_ = {"payload": True}
+        mock_get_json.return_value = dict_
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, dict_)
+        mock_get_json.assert_called_once_with(
+            f"https://api.github.com/orgs/{org_name}"
+        )
