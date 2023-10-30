b70f8c74ba53dbe128b39e2010ab71faf8feb12e
Fatimah1403 <fattyhassan14@gmail.com>
2023-10-30 23:55:18 +0000

    integration testing

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -107,7 +107,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_class.org, self.org_payload)
         self.assertEqual(test_class.repos_payload, self.repos_payload)
         self.assertEqual(test_class.public_repos(), self.expected_repos)
-        self.assertEqual(test_class.public_repos("TAVISH"), [])
+        self.assertEqual(test_class.public_repos("FATIMAH01"), [])
         self.mock.assert_called()
 
     def test_public_repos_with_license(self):
@@ -115,7 +115,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         test_class = GithubOrgClient("google")
         self.assertEqual(test_class.org, self.org_payload)
         self.assertEqual(test_class.public_repos(), self.expected_repos)
-        self.assertEqual(test_class.public_repos("TAVISH"), [])
+        self.assertEqual(test_class.public_repos("FATIMAH01"), [])
         self.assertEqual(test_class.public_repos(
             "apache-2.0"), self.apache2_repos)
         self.mock.assert_called()
